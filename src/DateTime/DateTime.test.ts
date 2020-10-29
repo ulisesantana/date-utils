f6fe8440a4da3358be6bdb826a209fb8e4e536c1
Ulises Santana <ulisesantana@gmail.com>
2020-10-29 22:56:59 +0000

    ✨ add nextDay and nextMonth to DateTime class

---
+++ src/DateTime/DateTime.test.ts
@@ -0,0 +1,61 @@
+import { DateTime } from "./DateTime";
+
+describe("DateTime should", () => {
+  describe("be instantiated based on", () => {
+    it("a Date", () => {
+      expect(new DateTime(new Date())).toBeInstanceOf(DateTime);
+    });
+
+    it("a string", () => {
+      expect(new DateTime("2020-12-10")).toBeInstanceOf(DateTime);
+    });
+
+    it("a number of milliseconds", () => {
+      expect(new DateTime(1600000000)).toBeInstanceOf(DateTime);
+    });
+  });
+
+  describe("retrieve the next day", () => {
+    it("for a regular day", () => {
+      expect(new DateTime("2020-12-10").nextDay().toString()).toBe(
+        "2020-12-11T00:00:00.000Z"
+      );
+      expect(
+        new DateTime("2020-03-18T16:45:30.100Z").nextDay().toString()
+      ).toBe("2020-03-19T16:45:30.100Z");
+    });
+
+    it("for spring daylight savings day change", () => {
+      expect(new DateTime("2020-03-29").nextDay().toString()).toBe(
+        "2020-03-30T00:00:00.000Z"
+      );
+      expect(
+        new DateTime("2020-03-29T12:00:00.000Z").nextDay().toString()
+      ).toBe("2020-03-30T12:00:00.000Z");
+    });
+
+    it("for spring daylight savings day change", () => {
+      expect(
+        new DateTime("2020-10-25T12:00:00.000Z").nextDay().toString()
+      ).toBe("2020-10-26T12:00:00.000Z");
+    });
+  });
+
+  it("retrieve the next month", () => {
+    expect(
+      new DateTime("2020-10-25T12:00:00.000Z").nextMonth().toString()
+    ).toBe("2020-11-25T12:00:00.000Z");
+    expect(
+      new DateTime("2020-04-15T12:00:00.000Z").nextMonth().toString()
+    ).toBe("2020-05-15T12:00:00.000Z");
+    expect(
+      new DateTime("2020-10-31T12:00:00.000Z").nextMonth().toString()
+    ).toBe("2020-11-30T12:00:00.000Z");
+    expect(
+      new DateTime("2020-01-20T12:00:00.000Z").nextMonth().toString()
+    ).toBe("2020-02-20T12:00:00.000Z");
+    expect(
+      new DateTime("2020-12-20T12:00:00.000Z").nextMonth().toString()
+    ).toBe("2021-01-20T12:00:00.000Z");
+  });
+});
---
+++ src/DateTime/DateTime.ts
@@ -0,0 +1,63 @@
+export class DateTime {
+  private readonly date: Date;
+  private readonly oneDayInMs = 86400000;
+  private readonly oneHourInMs = 3600000;
+
+  constructor(date: Date | string | number) {
+    this.date = new Date(date);
+  }
+
+  static create(date: Date | string | number): DateTime {
+    return new DateTime(date);
+  }
+
+  nextDay(): DateTime {
+    const moment = DateTime.create(this.date);
+    moment.date.setDate(moment.date.getDate() + 1);
+
+    const diff = moment.date.getTime() - this.date.getTime();
+    if (diff > this.oneDayInMs) {
+      return DateTime.create(moment.date.getTime() - this.oneHourInMs);
+    }
+
+    if (diff < this.oneDayInMs) {
+      return DateTime.create(moment.date.getTime() + this.oneHourInMs);
+    }
+
+    return moment;
+  }
+
+  nextMonth(): DateTime {
+    const nextMonthTotalDays = this.getNextMonthTotalDays();
+    const moment = DateTime.create(
+      this.date.getTime() + this.oneDayInMs * nextMonthTotalDays
+    );
+
+    const diff = moment.date.getDate() - this.date.getDate();
+    const nextMonthHasLessDays = this.date.getDate() < nextMonthTotalDays;
+
+    if (diff > 0) {
+      return DateTime.create(moment.date.getTime() - this.oneDayInMs * diff);
+    }
+
+    if (diff < 0 && nextMonthHasLessDays) {
+      return DateTime.create(
+        moment.date.getTime() + this.oneDayInMs * diff * -1
+      );
+    }
+
+    return moment;
+  }
+
+  toString(): string {
+    return this.date.toISOString();
+  }
+
+  private getNextMonthTotalDays(): number {
+    return new Date(
+      this.date.getFullYear(),
+      this.date.getMonth() + 2,
+      0
+    ).getDate();
+  }
+}
