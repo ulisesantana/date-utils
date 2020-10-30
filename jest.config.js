d2a6486d4d9d8d83bcd7bbcea4dec1571386a070
Ulises Santana <ulisesantana@gmail.com>
2020-10-30 21:47:14 +0000

    🔧 add base url for more descriptive imports

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   roots: ["<rootDir>/src"],
+  moduleDirectories: ["node_modules", "src"],
   testMatch: ["**/__tests__/**/*.+(ts|js)", "**/?(*.)+(spec|test).+(ts|js)"],
   transform: {
     "^.+\\.(ts)?$": "ts-jest",
